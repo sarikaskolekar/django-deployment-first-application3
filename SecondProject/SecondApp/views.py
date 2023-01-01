19c392824ef489f152f11aa91b04a227866dec27
sarikaskolekar <sarikaskolekar96@gmail.com>
2023-01-01 17:12:27 +0530

    firstcommit

--- SecondProject/SecondApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def students1(request):
-    return render(request,'SecondApp/students1.html');
-def students2(request):
-    return render(request,'SecondApp/students2.html');
